6b3ba969593f500ae3401db1ffbf15e74655f755
Sean Rahman <terturlminecraft@gmail.com>
2020-12-02 20:06:52 -0800

    Added example of JFrame

---
+++ src/main/java/com/Archangels/ProjectSierra/ProjectSierra.java
@@ -0,0 +1,15 @@
+package com.Archangels.ProjectSierra;
+
+import javax.swing.JFrame;
+
+public class ProjectSierra {
+
+	public static void main(String[] args) {
+		JFrame test = new JFrame("Example JFrame");
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		test.setLocationRelativeTo(null);
+		test.setResizable(false);
+		test.setSize(800, 800);
+	}
+	
+}
