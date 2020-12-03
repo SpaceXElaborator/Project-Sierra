3e4e4cc12594c8c00f81eb7937edf0661b10f7a3
Sean Rahman <terturlminecraft@gmail.com>
2020-12-03 07:41:13 -0800

    Added ability to get coordinate positions of items as well as velocities

---
+++ src/main/java/com/Archangels/ProjectSierra/Util/Direction.java
@@ -0,0 +1,21 @@
+package com.Archangels.ProjectSierra.Util;
+
+public class Direction {
+
+	private double dx;
+	private double dy;
+	
+	public Direction(double dx, double dy) {
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public double getX() {
+		return dx;
+	}
+	
+	public double getY() {
+		return dy;
+	}
+	
+}
