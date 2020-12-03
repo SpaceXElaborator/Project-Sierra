9b4bef22520a1175da70f8c3d79d3f76d596a4b7
Sean Rahman <terturlminecraft@gmail.com>
2020-12-03 04:04:13 -0800

    Code was commented out so as to fix some issues

--- src/main/java/com/Archangels/ProjectSierra/Physics/Gravity.java
+++ src/main/java/com/Archangels/ProjectSierra/Physics/Gravity.java
@@ -1,19 +1,24 @@
 package com.Archangels.ProjectSierra.Physics;
-private static final double GRAVITY = 9.8;
-private static final double TERMINAL_VELOCITY = 100;
-private double speed;
-private int current_y;
 
-public class Gravity() {
-	
-    speed = speed + GRAVITY;
+public class Gravity {
 
-    if (speed > TERMINAL_VELOCITY) { speed = TERMINAL_VELOCITY; }
+//	private static final double GRAVITY = 9.8;
+//	private static final double TERMINAL_VELOCITY = 100;
+//	private double speed;
+//	private int current_y;
+//
+//	public Gravity() {
+//		speed = speed + GRAVITY;
+//
+//		if (speed > TERMINAL_VELOCITY) {
+//			speed = TERMINAL_VELOCITY;
+//		}
+//
+//		if (current_y >= bottomOfScreen) {
+//			// Bounce effect
+//			speed = -speed / 4;
+//		}
+//		current_y += speed;
+//	}
 
-    if (current_y >= bottomOfScreen)
-    {
-    //Bounce effect
-     speed = -speed/4; 
-    } 
-    current_y += speed; 
 }
