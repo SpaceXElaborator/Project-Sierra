96e5200f4d1394ca39fc1cae836623d3e20d647c
Sean Rahman <terturlminecraft@gmail.com>
2020-12-04 15:14:03 -0800

    Restructured how the imageicon was set

--- src/main/java/com/Archangels/ProjectSierra/Entities/EntityPlayer.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/EntityPlayer.java
@@ -1,12 +1,12 @@
 package com.Archangels.ProjectSierra.Entities;
 
 import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
+import com.Archangels.ProjectSierra.ProjectSierra;
 import com.Archangels.ProjectSierra.Entities.Player.Classes;
 import com.Archangels.ProjectSierra.Entities.Projectiles.Projectile;
 import com.Archangels.ProjectSierra.Util.Direction;
@@ -17,21 +17,9 @@ public class EntityPlayer extends Controllable implements ProjectileSource {
 	
 	private Classes playerClass;
 	
-	public EntityPlayer(Location loc, Classes c) {
-		super(loc);
+	public EntityPlayer(Location loc, Classes c) throws IOException {
+		super(loc, new ImageIcon(ImageIO.read(ProjectSierra.class.getResource(c.getImage())).getScaledInstance(64, 128, Image.SCALE_SMOOTH)));
 		playerClass = c;
-		
-		ImageIcon icon = null;
-		BufferedImage img;
-		try {
-			img = ImageIO.read(getClass().getResource(c.getImage()));
-			Image dimb = img.getScaledInstance(64, 128, Image.SCALE_SMOOTH);
-			icon = new ImageIcon(dimb);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		setTexture(icon);
 	}
 	
 	public void teleport() {
@@ -42,12 +30,6 @@ public class EntityPlayer extends Controllable implements ProjectileSource {
 
 	public Classes getPlayerClass() {
 		return playerClass;
-	}
-	
-	@Override
-	public void createCollisionBox() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public <T extends Projectile> void fireProjectile(Class<? extends T> projectile) {
