ad107d77cc3e387adf0ba4ed9f5020f28d3fd8b7
Sean Rahman <terturlminecraft@gmail.com>
2020-12-03 22:47:03 -0800

    Added KeyListener and moved Handler to here

--- src/main/java/com/Archangels/ProjectSierra/Window/GameLauncher.java
+++ src/main/java/com/Archangels/ProjectSierra/Window/GameLauncher.java
@@ -3,21 +3,29 @@ package com.Archangels.ProjectSierra.Window;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.Archangels.ProjectSierra.Engine.GameKeyListener;
+import com.Archangels.ProjectSierra.Engine.Handler;
+
 // Main game window (Main Menu. Will make the Exit and Start Game button)
 public class GameLauncher extends JFrame {
 
 	// Needed for the creation of this class. This is a random serialNumber
 	private static final long serialVersionUID = -3645753859634135062L;
 	private JPanel panel;
+	private Handler handler;
 	
 	public GameLauncher() {
+		handler = new Handler();
 		setTitle("Project Sierra");
 		
 		if(panel == null) panel = new StartMenu(1280, 720);
 		setContentPane(panel);
 		
+		addKeyListener(new GameKeyListener(handler));
+		
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setFocusTraversalKeysEnabled(false);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setVisible(true);
@@ -26,6 +34,10 @@ public class GameLauncher extends JFrame {
 	public void setPane(JPanel panel) {
 		setContentPane(panel);
 		revalidate();
+	}
+
+	public Handler getHandler() {
+		return handler;
 	}
 	
 }
