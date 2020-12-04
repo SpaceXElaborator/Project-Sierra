6d189776fc48e2eacecdc6944c1974349af576c8
Sean Rahman <terturlminecraft@gmail.com>
2020-12-04 01:22:52 -0800

    Added classes support and framework

---
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Player/Classes.java
@@ -0,0 +1,23 @@
+package com.Archangels.ProjectSierra.Entities.Player;
+
+public enum Classes {
+
+	KNIGHT("Knight", "../Images/knight.png");
+
+	private String name;
+	private String image;
+	
+	Classes(String name, String image) {
+		this.name = name;
+		this.image = image;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+}
