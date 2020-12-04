b4bed41c33d84e5dc810d5577cbc968498c61bec
Sean Rahman <terturlminecraft@gmail.com>
2020-12-03 22:46:44 -0800

    Moved handler to GameLauncher.

--- src/main/java/com/Archangels/ProjectSierra/Window/GamePanel.java
+++ src/main/java/com/Archangels/ProjectSierra/Window/GamePanel.java
@@ -5,27 +5,33 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import javax.swing.ImageIcon;
+
+import com.Archangels.ProjectSierra.ProjectSierra;
 import com.Archangels.ProjectSierra.Engine.GameLoop;
 import com.Archangels.ProjectSierra.Engine.Handler;
+import com.Archangels.ProjectSierra.Entities.Player;
+import com.Archangels.ProjectSierra.Util.Location;
 
 public class GamePanel extends GameLoop {
 
 	private static final long serialVersionUID = -4583618417099095835L;
-	private Handler handler;
+	private Handler handler = ProjectSierra.getLauncher().getHandler();
 	
 	public GamePanel() {
-		handler = new Handler();
+		setPreferredSize(new Dimension(1280, 720));
 		
-		this.requestFocus();
-		this.setPreferredSize(new Dimension(1280, 720));
-		this.setBackground(Color.WHITE);
-		
+		Player p = new Player(new Location(200, 200), new ImageIcon(getClass().getResource("../Images/Untitled-1.png")));
+		handler.addGameElement(p);
+		handler.setControlledEntity(p);
 		new Thread(this).start();
+		
+		this.setFocusTraversalKeysEnabled(false);
 	}
 
 	@Override
-	public void update(double elapsed) {
-		handler.update(elapsed);
+	public void update() {
+		handler.update();
 	}
 	
 	@Override
@@ -33,6 +39,9 @@ public class GamePanel extends GameLoop {
 		Graphics2D test = (Graphics2D) g;
 		
 		// Background stuff here
+		test.setColor(Color.BLACK);
+		test.fillRect(0, 0, 1280, 720);
+		test.drawRect(0, 0, 1280, 720);
 		
 		// Everything else
 		handler.render(g);
