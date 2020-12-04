82f9eda2e601959c8bc1c7bc2a3b95d5e7130fc1
Sean Rahman <terturlminecraft@gmail.com>
2020-12-03 22:45:11 -0800

    Added Velocity Support

--- src/main/java/com/Archangels/ProjectSierra/Entities/Entity.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Entity.java
@@ -3,15 +3,18 @@ package com.Archangels.ProjectSierra.Entities;
 import javax.swing.ImageIcon;
 
 import com.Archangels.ProjectSierra.Engine.GameElement;
+import com.Archangels.ProjectSierra.Util.Direction;
 import com.Archangels.ProjectSierra.Util.Location;
 
 public abstract class Entity implements GameElement {
 
 	private Location loc;
+	private Direction dir;
 	private ImageIcon i;
 	
 	public Entity(Location loc) {
 		this.loc = loc;
+		this.dir = new Direction(0, 0);
 	}
 	
 	public void setTexture(ImageIcon i) {
@@ -28,6 +31,10 @@ public abstract class Entity implements GameElement {
 	
 	public Location getLocation() {
 		return this.loc;
+	}
+	
+	public Direction getDirection() {
+		return dir;
 	}
 	
 }
