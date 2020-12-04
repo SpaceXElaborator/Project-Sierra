d6063bb29e7e8d54b14452ca14345bafadddfbe0
Sean Rahman <terturlminecraft@gmail.com>
2020-12-04 01:22:01 -0800

    Changed to EntityPlayer naming convention. Added classes and the ability to flip an image dependant on direction

--- src/main/java/com/Archangels/ProjectSierra/Entities/EntityPlayer.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/EntityPlayer.java
@@ -1,17 +1,37 @@
 package com.Archangels.ProjectSierra.Entities;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
+import com.Archangels.ProjectSierra.Entities.Player.Classes;
 import com.Archangels.ProjectSierra.Entities.Projectiles.Projectile;
 import com.Archangels.ProjectSierra.Util.Direction;
 import com.Archangels.ProjectSierra.Util.Location;
 import com.Archangels.ProjectSierra.Util.Velocity;
 
-public class Player extends Controllable implements ProjectileSource {
+public class EntityPlayer extends Controllable implements ProjectileSource {
 	
-	public Player(Location loc, ImageIcon i) {
+	private Classes playerClass;
+	
+	public EntityPlayer(Location loc, Classes c) {
 		super(loc);
-		setTexture(i);
+		playerClass = c;
+		
+		ImageIcon icon = null;
+		BufferedImage img;
+		try {
+			img = ImageIO.read(getClass().getResource(c.getImage()));
+			Image dimb = img.getScaledInstance(64, 128, Image.SCALE_SMOOTH);
+			icon = new ImageIcon(dimb);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		setTexture(icon);
 	}
 	
 	public void teleport() {
@@ -20,6 +40,10 @@ public class Player extends Controllable implements ProjectileSource {
 		if(getVelocity().getDirection() == Direction.RIGHT) getLocation().setX(getLocation().getX() + 60);
 	}
 
+	public Classes getPlayerClass() {
+		return playerClass;
+	}
+	
 	@Override
 	public void createCollisionBox() {
 		// TODO Auto-generated method stub
