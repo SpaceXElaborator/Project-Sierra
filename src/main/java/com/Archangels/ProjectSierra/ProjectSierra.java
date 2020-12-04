9d886175fcaada81603392d14f353ff8998193a3
Sean Rahman <terturlminecraft@gmail.com>
2020-12-04 15:10:48 -0800

    Added --debug argument

--- src/main/java/com/Archangels/ProjectSierra/ProjectSierra.java
+++ src/main/java/com/Archangels/ProjectSierra/ProjectSierra.java
@@ -5,13 +5,22 @@ import com.Archangels.ProjectSierra.Window.GameLauncher;
 public class ProjectSierra {
 
 	private static GameLauncher launch;
+	private static boolean debug = false;
 	
 	public static void main(String[] args) {
+		if(args.length > 0) {
+			if(args[0].equalsIgnoreCase("--debug")) debug = true;
+		}
+		
 		launch = new GameLauncher();
 	}
 	
 	public static GameLauncher getLauncher() {
 		return launch;
+	}
+	
+	public static boolean isDebug() {
+		return debug;
 	}
 	
 }
