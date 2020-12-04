afbf2ac6ab42e7464f9d9903868d44862f56d9cc
GOD-KUN\brend <brendonmicheal13@gmail.com>
2020-12-04 00:18:38 -0800

    Changed Velocity and added support for a teleport move

--- src/main/java/com/Archangels/ProjectSierra/Entities/Player.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Player.java
@@ -1,31 +1,39 @@
 package com.Archangels.ProjectSierra.Entities;
 
-import java.awt.Graphics;
-
 import javax.swing.ImageIcon;
 
+import com.Archangels.ProjectSierra.Entities.Projectiles.Projectile;
 import com.Archangels.ProjectSierra.Util.Direction;
 import com.Archangels.ProjectSierra.Util.Location;
+import com.Archangels.ProjectSierra.Util.Velocity;
 
-public class Player extends Controllable {
+public class Player extends Controllable implements ProjectileSource {
 	
 	public Player(Location loc, ImageIcon i) {
 		super(loc);
 		setTexture(i);
 	}
-
-	public void update() {
-		getLocation().setX(getLocation().getX() + getVelocity().getX());
-	}
-
-	public void render(Graphics g) {
-		g.drawImage(getTexture().getImage(), (int)getLocation().getX(), (int)getLocation().getY(), null);
-	}
 	
 	public void teleport() {
 		if(getVelocity().getDirection() == null) return;
-		if(getVelocity().getDirection() == Direction.LEFT) getLocation().setX(getLocation().getX() - 20);
-		if(getVelocity().getDirection() == Direction.RIGHT) getLocation().setX(getLocation().getX() + 20);
+		if(getVelocity().getDirection() == Direction.LEFT) getLocation().setX(getLocation().getX() - 60);
+		if(getVelocity().getDirection() == Direction.RIGHT) getLocation().setX(getLocation().getX() + 60);
+	}
+
+	@Override
+	public void createCollisionBox() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public <T extends Projectile> void fireProjectile(Class<? extends T> projectile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public <T extends Projectile> void fireProjectile(Class<? extends T> projectile, Velocity dir) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
