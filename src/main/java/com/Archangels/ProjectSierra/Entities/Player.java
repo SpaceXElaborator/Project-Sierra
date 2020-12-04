dd6763f33d8c16c3e3e5ce6965f24bfa6e0a24f3
Sean Rahman <terturlminecraft@gmail.com>
2020-12-03 22:55:11 -0800

    Added drawing and updating of the player

--- src/main/java/com/Archangels/ProjectSierra/Entities/Player.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Player.java
@@ -7,18 +7,18 @@ import javax.swing.ImageIcon;
 import com.Archangels.ProjectSierra.Util.Location;
 
 public class Player extends Controllable {
-
+	
 	public Player(Location loc, ImageIcon i) {
 		super(loc);
 		setTexture(i);
 	}
 
-	public void update(double elapsed) {
-		
+	public void update() {
+		getLocation().setX(getLocation().getX() + getDirection().getX());
 	}
 
 	public void render(Graphics g) {
-		
+		g.drawImage(getTexture().getImage(), (int)getLocation().getX(), (int)getLocation().getY(), null);
 	}
 	
 }
