91430330545b93ce62cc8b0aa5a0ccd132bf2ade
Sean Rahman <terturlminecraft@gmail.com>
2020-12-04 01:21:08 -0800

    Added ability to change move speed

--- src/main/java/com/Archangels/ProjectSierra/Entities/Controllable.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Controllable.java
@@ -6,6 +6,8 @@ import com.Archangels.ProjectSierra.Util.Location;
 
 public abstract class Controllable extends Entity {
 
+	private double movespeed = 5;
+	
 	public Controllable(Location loc) {
 		super(loc);
 	}
@@ -13,9 +15,17 @@ public abstract class Controllable extends Entity {
 	public void update() {
 		getLocation().setX(getLocation().getX() + getVelocity().getX());
 	}
-
+	
 	public void render(Graphics g) {
 		g.drawImage(getTexture().getImage(), (int)getLocation().getX(), (int)getLocation().getY(), null);
+	}
+	
+	public double getMoveSpeed() {
+		return movespeed;
+	}
+	
+	public void setMoveSpeed(double d) {
+		movespeed = d;
 	}
 	
 }
