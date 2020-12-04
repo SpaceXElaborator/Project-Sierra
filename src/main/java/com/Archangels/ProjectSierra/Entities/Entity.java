5f2596266e169ddbd52270c7c319c2eca02fa2d4
Sean Rahman <terturlminecraft@gmail.com>
2020-12-04 01:23:37 -0800

    Added setDirection() to be able to flip image dependent on direction

--- src/main/java/com/Archangels/ProjectSierra/Entities/Entity.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Entity.java
@@ -1,10 +1,17 @@
 package com.Archangels.ProjectSierra.Entities;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
 import javax.swing.ImageIcon;
 
 import com.Archangels.ProjectSierra.Engine.GameElement;
-import com.Archangels.ProjectSierra.Util.Velocity;
+import com.Archangels.ProjectSierra.Util.Direction;
 import com.Archangels.ProjectSierra.Util.Location;
+import com.Archangels.ProjectSierra.Util.Velocity;
 
 public abstract class Entity implements GameElement {
 
@@ -32,6 +39,22 @@ public abstract class Entity implements GameElement {
 	
 	public Location getLocation() {
 		return this.loc;
+	}
+	
+	public void setDirection(Direction dir) {
+		if(getVelocity().getDirection() == dir) return;
+		Image image = i.getImage();
+		BufferedImage img = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+		Graphics g = img.createGraphics();
+		g.drawImage(image, 0, 0, null);
+		g.dispose();
+		AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+		tx.translate(-image.getWidth(null), 0);
+		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		img = op.filter(img, null);
+		
+		i = new ImageIcon(img);
+		getVelocity().setDirection(dir);
 	}
 	
 	public Velocity getVelocity() {
