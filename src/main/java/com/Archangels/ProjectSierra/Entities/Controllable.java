a069b67764c8247c0a74977ec9e490cf46efab13
GOD-KUN\brend <brendonmicheal13@gmail.com>
2020-12-04 00:18:03 -0800

    Added update methods for controllable

--- src/main/java/com/Archangels/ProjectSierra/Entities/Controllable.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Controllable.java
@@ -1,11 +1,21 @@
 package com.Archangels.ProjectSierra.Entities;
 
+import java.awt.Graphics;
+
 import com.Archangels.ProjectSierra.Util.Location;
 
 public abstract class Controllable extends Entity {
 
 	public Controllable(Location loc) {
 		super(loc);
+	}
+	
+	public void update() {
+		getLocation().setX(getLocation().getX() + getVelocity().getX());
+	}
+
+	public void render(Graphics g) {
+		g.drawImage(getTexture().getImage(), (int)getLocation().getX(), (int)getLocation().getY(), null);
 	}
 	
 }
