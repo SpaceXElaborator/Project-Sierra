f085e57b57eb327b08aa7a90883b407f56471c48
GOD-KUN\brend <brendonmicheal13@gmail.com>
2020-12-04 00:17:41 -0800

    Changed Entity to instance of Controllable

--- src/main/java/com/Archangels/ProjectSierra/Engine/GameKeyListener.java
+++ src/main/java/com/Archangels/ProjectSierra/Engine/GameKeyListener.java
@@ -3,8 +3,8 @@ package com.Archangels.ProjectSierra.Engine;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import com.Archangels.ProjectSierra.Entities.Controllable;
 import com.Archangels.ProjectSierra.Entities.Entity;
-import com.Archangels.ProjectSierra.Entities.Player;
 import com.Archangels.ProjectSierra.Util.Direction;
 
 public class GameKeyListener extends KeyAdapter {
@@ -19,7 +19,7 @@ public class GameKeyListener extends KeyAdapter {
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		if(handler.getControlledEntity() != null) {
-			Player ent = (Player) handler.getControlledEntity();
+			Controllable ent = (Controllable) handler.getControlledEntity();
 			if(key == KeyEvent.VK_RIGHT) {
 				ent.getVelocity().setDirection(Direction.RIGHT);
 				ent.getVelocity().setX(5);
@@ -30,9 +30,12 @@ public class GameKeyListener extends KeyAdapter {
 				ent.getVelocity().setX(-5);
 			}
 			
-			if(key == KeyEvent.VK_X) {
-				ent.teleport();
-			}
+//			if(key == KeyEvent.VK_X) {
+//				if(ent instanceof Player) {
+//					Player p = (Player)ent;
+//					p.teleport();
+//				}
+//			}
 		}
 	}
 	
