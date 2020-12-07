a607486e4558815df8410e2fcbbfad97b0acd4a3
Sean Rahman <terturlminecraft@gmail.com>
2020-12-06 17:55:52 -0800

    Added a scheduler service for controls in time

--- src/main/java/com/Archangels/ProjectSierra/ProjectSierra.java
+++ src/main/java/com/Archangels/ProjectSierra/ProjectSierra.java
@@ -1,5 +1,8 @@
 package com.Archangels.ProjectSierra;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 import com.Archangels.ProjectSierra.Engine.KeyBindings;
 import com.Archangels.ProjectSierra.Window.GameLauncher;
 
@@ -8,6 +11,7 @@ public class ProjectSierra {
 	private static GameLauncher launch;
 	private static boolean debug = false;
 	private static KeyBindings bindings;
+	private static ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
 	
 	public static void main(String[] args) {
 		if(args.length > 0) {
@@ -25,6 +29,10 @@ public class ProjectSierra {
 	
 	public static KeyBindings getKeyBindings() {
 		return bindings;
+	}
+	
+	public static ScheduledExecutorService getScheduler() {
+		return service;
 	}
 	
 	public static boolean isDebug() {
