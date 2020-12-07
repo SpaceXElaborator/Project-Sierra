97362f3a9a6517f05ea0e3e85ee898418ae91d4b
Sean Rahman <terturlminecraft@gmail.com>
2020-12-06 18:08:43 -0800

    Added the ability to use any Usable item

--- src/main/java/com/Archangels/ProjectSierra/Entities/Controllable.java
+++ src/main/java/com/Archangels/ProjectSierra/Entities/Controllable.java
@@ -4,6 +4,7 @@ import javax.swing.ImageIcon;
 
 import com.Archangels.ProjectSierra.Engine.CameraControl.Camera;
 import com.Archangels.ProjectSierra.Entities.Projectiles.Projectile;
+import com.Archangels.ProjectSierra.Item.Usable;
 import com.Archangels.ProjectSierra.Util.Location;
 import com.Archangels.ProjectSierra.Util.Velocity;
 
@@ -11,6 +12,8 @@ public abstract class Controllable extends Entity implements ProjectileSource {
 
 	private double movespeed = 5;
 	private Camera camera;
+	private Usable mainHandItem;
+	private Usable offHandItem;
 	
 	public Controllable(Location loc, ImageIcon i) {
 		super(loc, i);
@@ -27,6 +30,22 @@ public abstract class Controllable extends Entity implements ProjectileSource {
 	
 	public Camera getCamera() {
 		return camera;
+	}
+	
+	public Usable getMainHand() {
+		return mainHandItem;
+	}
+	
+	public void setMainHand(Usable u) {
+		mainHandItem = u;
+	}
+	
+	public Usable getOffHandItem() {
+		return offHandItem;
+	}
+	
+	public void setOffHandItem(Usable u) {
+		offHandItem = u;
 	}
 	
 	public <T extends Projectile> void fireProjectile(T projectile) {
