c44b8fda55c6de588acc12c6824bf97057e0d04c
Sean Rahman <terturlminecraft@gmail.com>
2020-12-06 18:09:29 -0800

    Ability to use Usables dependant on which mouse button pressed

--- src/main/java/com/Archangels/ProjectSierra/Engine/Mouse/GameMouseListener.java
+++ src/main/java/com/Archangels/ProjectSierra/Engine/Mouse/GameMouseListener.java
@@ -3,22 +3,15 @@ package com.Archangels.ProjectSierra.Engine.Mouse;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.ImageIcon;
-
 import com.Archangels.ProjectSierra.ProjectSierra;
 import com.Archangels.ProjectSierra.Engine.Handler;
-import com.Archangels.ProjectSierra.Entities.Controllable;
-import com.Archangels.ProjectSierra.Entities.Projectiles.Projectile;
-import com.Archangels.ProjectSierra.Util.Velocity;
 
 public class GameMouseListener implements MouseListener {
 	
-	private Controllable cont;
 	private Handler handler;
 	
 	public GameMouseListener() {
 		handler = ProjectSierra.getLauncher().getHandler();
-		cont = (Controllable) handler.getControlledEntity();
 	}
 
 	public void mouseReleased(MouseEvent e) {
@@ -39,9 +32,13 @@ public class GameMouseListener implements MouseListener {
 
 	public void mouseClicked(MouseEvent e) {
 		if(e.getButton() == MouseEvent.BUTTON1) {
-			Projectile p = new Projectile(new ImageIcon(ProjectSierra.class.getResource("Images/Projectiles/Bullet.png")), new Velocity(10, 0), handler.getControlledEntity().getLocation());
-			cont.fireProjectile(p);
-			System.out.println(e.getX() + " | " + e.getY());
+			if(handler.getControlledEntity().getMainHand() != null)
+				handler.getControlledEntity().getMainHand().onLeftClick();
+		}
+		
+		if(e.getButton() == MouseEvent.BUTTON3) {
+			if(handler.getControlledEntity().getMainHand() != null)
+				handler.getControlledEntity().getMainHand().onRightClick();
 		}
 	}
 	
