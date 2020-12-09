f5e13449001d46728fee406fb1d0dd9108d59bf8
GOD-KUN\brend <brendonmicheal13@gmail.com>
2020-12-09 13:30:33 -0800

    Removed Mouse listener and fixed issued with booting up the game

--- src/main/java/com/Archangels/ProjectSierra/Engine/MouseListener.java
+++
@@ -1,5 +0,0 @@
-package com.Archangels.ProjectSierra.Engine;
-
-public class MouseListener {
-
-}
--- src/main/java/com/Archangels/ProjectSierra/Item/Weapons/Gun.java
+++ src/main/java/com/Archangels/ProjectSierra/Item/Weapons/Gun.java
@@ -18,32 +18,32 @@ public class Gun extends Weapon {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public void onLeftClick() {
 		Controllable cont = ProjectSierra.getLauncher().getHandler().getControlledEntity();
 		Projectile p = new Projectile(new ImageIcon(ProjectSierra.class.getResource("Images/Projectiles/Bullet.png")), new Velocity(10, 0), cont.getLocation());
 		cont.fireProjectile(p);
 	}
 
-	@Override
+	
 	public void onRightClick() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+	
 	public Location getLocation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	
 	public void update() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+	
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
 		
