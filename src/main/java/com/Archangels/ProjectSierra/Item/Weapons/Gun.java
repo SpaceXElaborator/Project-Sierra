a4b389f57427dfbad7c6457d3195bbdcac045598
Sean Rahman <terturlminecraft@gmail.com>
2020-12-09 14:28:59 -0800

    Removed weird generated lines from SourceTree

--- src/main/java/com/Archangels/ProjectSierra/Item/Weapons/Gun.java
+++ src/main/java/com/Archangels/ProjectSierra/Item/Weapons/Gun.java
@@ -17,44 +17,24 @@ public class Gun extends Weapon {
 		super(s, j);
 	}
 
-<<<<<<< HEAD
-=======
-	
->>>>>>> c73c023df5bc5efc8336dd3015dac2ffe776f90a
 	public void onLeftClick() {
 		Controllable cont = ProjectSierra.getLauncher().getHandler().getControlledEntity();
 		Projectile p = new Projectile(new ImageIcon(ProjectSierra.class.getResource("Images/Projectiles/Bullet.png")), new Velocity(10, 0), cont.getLocation());
 		cont.fireProjectile(p);
 	}
 
-<<<<<<< HEAD
-=======
-	
->>>>>>> c73c023df5bc5efc8336dd3015dac2ffe776f90a
 	public void onRightClick() {
 		
 	}
 
-<<<<<<< HEAD
-=======
-	
->>>>>>> c73c023df5bc5efc8336dd3015dac2ffe776f90a
 	public Location getLocation() {
 		return null;
 	}
 
-<<<<<<< HEAD
-=======
-	
->>>>>>> c73c023df5bc5efc8336dd3015dac2ffe776f90a
 	public void update() {
 		
 	}
 
-<<<<<<< HEAD
-=======
-	
->>>>>>> c73c023df5bc5efc8336dd3015dac2ffe776f90a
 	public void render(Graphics g) {
 		
 	}
