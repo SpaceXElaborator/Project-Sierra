0d2b4c5ea1e3ef8b61f4913530ede586df020085
GOD-KUN\brend <brendonmicheal13@gmail.com>
2020-12-09 18:14:49 -0800

    Template for sword item

---
+++ src/main/java/com/Archangels/ProjectSierra/Item/Weapons/Sword.java
@@ -0,0 +1,35 @@
+package com.Archangels.ProjectSierra.Item.Weapons;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import com.Archangels.ProjectSierra.Item.Weapon;
+import com.Archangels.ProjectSierra.Util.Location;
+
+
+public class Sword extends Weapon {
+	
+	public Sword(String s, ImageIcon j) {
+		super(s, j);
+	}
+	
+	public void onLeftClick() {
+		
+	}
+	
+	public void onRightClick() {
+		
+	}
+	
+	public Location getLocation() {
+		return null;
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+
+}
