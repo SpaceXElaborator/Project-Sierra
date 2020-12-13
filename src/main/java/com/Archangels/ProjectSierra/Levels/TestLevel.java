7129123d12e930a60e8a389094fdb600d146e616
Sean Rahman <terturlminecraft@gmail.com>
2020-12-13 12:56:14 -0800

    Added Chest to level

--- src/main/java/com/Archangels/ProjectSierra/Levels/TestLevel.java
+++ src/main/java/com/Archangels/ProjectSierra/Levels/TestLevel.java
@@ -1,5 +1,6 @@
 package com.Archangels.ProjectSierra.Levels;
 
+import com.Archangels.ProjectSierra.Block.BackgroundBlock;
 import com.Archangels.ProjectSierra.Block.BasicGround;
 import com.Archangels.ProjectSierra.Block.PassableBlock;
 import com.Archangels.ProjectSierra.Engine.Handler;
@@ -9,55 +10,56 @@ public class TestLevel {
 
 	public TestLevel(Handler handler) {
 		/// Floor
-				for (int i = 0; i < 1240; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(i, 680)));
-				}
-				// Left wall
-				for (int i = 0; i < 700; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(0, i)));
-				}
-				// Right Wall
-				for (int i = 0; i < 700; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(1240, i)));
-				}
-				// Far-Left Column
-				for (int i = 520; i < 700; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(40, i)));
-				}
-				// Left-Middle column
-				for (int i = 560; i < 700; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(80, i)));
-				}
-				// Right-middle column
-				for (int i = 600; i < 700; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(120, i)));
-				}
+		for (int i = 0; i < 1240; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(i, 680)));
+		}
+		
+		// Left wall
+		for (int i = 0; i < 700; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(0, i)));
+		}
+		// Right Wall
+		for (int i = 0; i < 700; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(1240, i)));
+		}
+		// Far-Left Column
+		for (int i = 520; i < 700; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(40, i)));
+		}
+		// Left-Middle column
+		for (int i = 560; i < 700; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(80, i)));
+		}
+		// Right-middle column
+		for (int i = 600; i < 700; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(120, i)));
+		}
 
-				// Far right column
-				for (int i = 640; i < 700; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(160, i)));
-				}
+		// Far right column
+		handler.addGameElement(new BasicGround(new Location(160, 640)));
+		
+		handler.addGameElement(new BackgroundBlock(new Location(280, 640), 40, 40));
 
-				// Hanging single block on left wall
-				handler.addGameElement(new BasicGround(new Location(40, 320)));
+		// Hanging single block on left wall
+		handler.addGameElement(new BasicGround(new Location(40, 320)));
 
-				// Middle left platform
-				for (int i = 280; i < 400; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(i, 400)));
-				}
+		// Middle left platform
+		for (int i = 280; i < 400; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(i, 400)));
+		}
 
-				// Middle top platform
-				for (int i = 540; i < 660; i += 40) {
-					handler.addGameElement(new PassableBlock(new Location(i, 200)));
-				}
-				
-				for (int i = 540; i < 660; i += 40) {
-					handler.addGameElement(new PassableBlock(new Location(i, 400)));
-				}
+		// Middle top platform
+		for (int i = 540; i < 660; i += 40) {
+			handler.addGameElement(new PassableBlock(new Location(i, 200), 40, 15));
+		}
 
-				for (int i = 900; i < 1020; i += 40) {
-					handler.addGameElement(new BasicGround(new Location(i, 400)));
-				}
+		for (int i = 540; i < 660; i += 40) {
+			handler.addGameElement(new PassableBlock(new Location(i, 400), 40, 15));
+		}
+
+		for (int i = 900; i < 1020; i += 40) {
+			handler.addGameElement(new BasicGround(new Location(i, 400)));
+		}
 	}
-	
+
 }
